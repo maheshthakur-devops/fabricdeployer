90bf89a20831a193140a69b0195725de7a06c555
maheshthakur-devops <68374489+maheshthakur-devops@users.noreply.github.com>
2020-09-28 13:51:22 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -24,9 +24,9 @@ copyright = u'2020, Mahesh Thakur'
 author = u'Mahesh Thakur'
 
 # The short X.Y version
-version = u''
+version = u'0.0.1'
 # The full version, including alpha/beta/rc tags
-release = u'v0.0.1'
+release = u'0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
